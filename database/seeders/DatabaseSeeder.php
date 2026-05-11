9c83946706b187974878e23fb86014b73216cb77
Uriel <uriellopezm7@gmail.com>
2026-05-11 17:40:19 -0600

    Completar DatabaseSeeder para que Railway no choque

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // Solo llamamos al que sí tiene código para evitar errores
+        $this->call([
+            ServiceSeeder::class,
+        ]);
+    }
+}
